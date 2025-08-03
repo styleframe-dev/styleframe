61c398e2cc50d4e76882142453227c6773ecace5
Alex Grozav <alex@grozav.com>
2025-08-03 16:48:38 +0200

    feat: add core package

--- config/vite/vite.config.d.ts
+++ config/vite/vite.config.d.ts
@@ -1,3 +1,6 @@
+/// <reference types="vite/client" />
+/// <reference types="vitest/globals" />
+
 import type { UserConfig } from "vite";
 
 export declare function createViteConfig(
--- config/vite/vite.config.js
+++ config/vite/vite.config.js
@@ -1,19 +1,25 @@
 import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
+import { configDefaults as vitestConfig } from "vitest/config";
 
 /**
  * @typedef {import('vite').UserConfig['build']['lib']} UserConfig
  */
-export const createViteConfig = (options = {}) =>
+export const createViteConfig = (cwd, options = {}) =>
 	defineConfig({
 		plugins: [dts({ rollupTypes: true })],
 		build: {
 			lib: {
-				entry: resolve(process.cwd(), "src/index.ts"),
+				entry: resolve(cwd, "src/index.ts"),
 				name: "styleframe",
 				fileName: "styleframe",
 				...options,
 			},
+		},
+		test: {
+			globals: true,
+			include: ["src/**/*.spec.{ts,tsx}"],
+			exclude: vitestConfig.exclude,
 		},
 	});
