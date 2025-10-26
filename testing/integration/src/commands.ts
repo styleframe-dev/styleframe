41469ff338b2d0bfffea9008828ea6d0835333fc
Alex Grozav <alex@grozav.com>
2025-10-26 11:07:37 +0200

    test: add integration testing package

---
+++ testing/integration/src/commands.ts
@@ -0,0 +1,76 @@
+import shell from "shelljs";
+import path from "path";
+
+export function buildPackages(cwd: string) {
+	shell.exec("pnpm run build:nodocs", { cwd });
+}
+
+function mapTarballsToPackages(
+	cwd: string,
+	files: string[],
+): Record<string, string> {
+	const scopePrefix = "@styleframe/";
+	const map: Record<string, string> = {};
+
+	files.forEach((file) => {
+		// Remove `.tgz` extension
+		const base = file.replace(/\.tgz$/, "");
+		// Extract package name without version
+		const name = base.replace(/-\d+\.\d+\.\d+$/, "");
+
+		// Convert scoped packages
+		const scopedName =
+			name === "styleframe"
+				? name
+				: `${scopePrefix}${name.replace(/^styleframe-/, "")}`;
+
+		map[scopedName] = path.join(cwd, file);
+	});
+
+	return map;
+}
+
+export function createPackageTarballs(
+	cwd: string,
+	{
+		filter = [],
+		outputDir = "dist",
+	}: { filter: string[]; outputDir?: string } = {},
+) {
+	shell.exec(
+		`pnpm pack --recursive --pack-destination ${outputDir} ${filter.map((f) => `--filter '${f}'`).join(" ")}`,
+		{
+			cwd,
+		},
+	);
+
+	const tarballCwd = `${cwd}/${outputDir}`;
+	const tarballs = shell
+		.exec(`ls *.tgz`, { cwd: tarballCwd })
+		.stdout.split("\n")
+		.filter(Boolean);
+
+	return mapTarballsToPackages(tarballCwd, tarballs);
+}
+
+export function createStarterVitePackage(
+	cwd: string,
+	{
+		template = "vanilla-ts",
+		outputDir = "tmp/vite",
+	}: {
+		template?: string;
+		outputDir?: string;
+	} = {},
+) {
+	shell.exec(`pnpm create vite --template ${template} ${outputDir}`, { cwd });
+}
+
+export function installStyleframeUsingCLI(
+	cwd: string,
+	packageToTarballMap: Record<string, string>,
+) {
+	shell.exec(`pnpm install -D ${packageToTarballMap["@styleframe/cli"]}`, cwd);
+
+	shell.exec("styleframe init", { cwd });
+}
---
+++ testing/integration/src/main.ts
@@ -0,0 +1,46 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import {
+	buildPackages,
+	createPackageTarballs,
+	createStarterVitePackage,
+	installStyleframeUsingCLI,
+} from "./commands";
+
+const __dirname = fileURLToPath(import.meta.url);
+
+const packageDir = path.join(__dirname, "..", "..");
+const workspaceDir = path.join(packageDir, "..", "..");
+
+/**
+ * 1. Build styleframe packages
+ */
+
+buildPackages(workspaceDir);
+
+/**
+ * 2. Generate .tgz package files
+ */
+
+const packageToTarballMap = createPackageTarballs(workspaceDir, {
+	filter: ["./engine", "./tooling", "./theme"],
+});
+
+/**
+ * 3. Create starter vite package
+ */
+
+const viteStyleframeCLIOutputDir = path.resolve(
+	packageDir,
+	"tmp/vite-styleframe-cli",
+);
+
+createStarterVitePackage(packageDir, {
+	outputDir: viteStyleframeCLIOutputDir,
+});
+
+/**
+ * 4. Install styleframe using the CLI
+ */
+
+installStyleframeUsingCLI(viteStyleframeCLIOutputDir, packageToTarballMap);
