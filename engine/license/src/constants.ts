3b5f09ecfe4c46eefffa551f1a3c1789017201f5
Alex Grozav <alex@grozav.com>
2025-11-09 13:52:50 +0200

    refactor: move license to pro

--- engine/license/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const LICENSE_PROPERTY_NAME = Symbol.for("__licenseRequired");
-
-export const SUPABASE_PROJECT_ID = "zkbxuglymolesrtagncn";
-export const SUPABASE_URL = `https://${SUPABASE_PROJECT_ID}.supabase.co`;
--- engine/license/vite.config.ts
+++
@@ -1,5 +0,0 @@
-import { createViteConfig } from "@styleframe/config-vite";
-
-const __dirname = new URL(".", import.meta.url).pathname;
-
-export default createViteConfig("license", __dirname);
