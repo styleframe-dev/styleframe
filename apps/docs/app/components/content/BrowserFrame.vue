6010b2d5800547a2f87b1eaea008cad9ee24c4a0
Alex Grozav <alex@grozav.com>
2025-11-22 20:14:19 +0200

    fix: fix browserframe padding

--- apps/docs/app/components/content/BrowserFrame.vue
+++ apps/docs/app/components/content/BrowserFrame.vue
@@ -1,28 +1,28 @@
 <script setup lang="ts">
 interface Props {
-	title?: string;
+  title?: string;
 }
 
 defineProps<Props>();
 </script>
 
 <template>
-	<div class="p-1 border border-default rounded text-sm bg-default/70">
-		<div
-			v-if="title"
-			class="flex items-center bg-slate-100 dark:bg-slate-800 p-2 rounded font-bold"
-		>
-			<span class="flex gap-1 absolute">
-				<span class="rounded-full bg-error inline-flex w-3 h-3" />
-				<span class="rounded-full bg-warning inline-flex w-3 h-3" />
-				<span class="rounded-full bg-success inline-flex w-3 h-3" />
-			</span>
-			<span class="flex w-full align-center justify-center">
-				{{ title }}
-			</span>
-		</div>
-		<div class="p-3 overflow-auto">
-			<slot mdc-unwrap="p" />
-		</div>
-	</div>
+  <div class="p-1 border border-default rounded text-sm bg-default/70">
+    <div
+      v-if="title"
+      class="flex items-center bg-slate-100 dark:bg-slate-800 p-2 rounded font-bold"
+    >
+      <span class="flex gap-1 absolute">
+        <span class="rounded-full bg-error inline-flex w-3 h-3" />
+        <span class="rounded-full bg-warning inline-flex w-3 h-3" />
+        <span class="rounded-full bg-success inline-flex w-3 h-3" />
+      </span>
+      <span class="flex w-full align-center justify-center">
+        {{ title }}
+      </span>
+    </div>
+    <div class="px-3 overflow-auto">
+      <slot mdc-unwrap="p" />
+    </div>
+  </div>
 </template>
