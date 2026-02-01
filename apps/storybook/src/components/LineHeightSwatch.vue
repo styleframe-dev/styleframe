96f68844d53b2a54e75a0499582b6dc563ae57fb
Alex Grozav <alex@grozav.com>
2026-02-01 08:09:57 +0200

    fix: format long line in LineHeightSwatch.vue

--- apps/storybook/src/components/LineHeightSwatch.vue
+++ apps/storybook/src/components/LineHeightSwatch.vue
@@ -8,7 +8,8 @@ const props = defineProps<{
 	value: string;
 }>();
 
-const sampleText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.";
+const sampleText =
+	"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.";
 </script>
 
 <template>
